214e50956ef6e8f3ecc408d58783d86cc6d5fbc5
Joshua-amare <41296823+Joshua-amare@users.noreply.github.com>
2019-02-11 01:32:47 +0800

    up and running finally

--- hang_man.py
+++ hang_man.py
@@ -1,5 +1,7 @@
 from random import randint
 
+temp = []
+var = 6
 
 #takes master input froom the game controller
 def get_word():
@@ -20,25 +22,40 @@ def display_val(val):
         dis.append("-")
     return dis
 
+
 #name game_play checks if the user has taken the input supplied
 def game_play(x,dis):
+
     #x is the hang man puzzle and dis is the - display
-    temp = []
     i = 0
     print("let the game begin but remeber you have to guess the correct words")
     chx = input(" please write a sigle guess only: ")
-    temp[i]= chx
+    temp.append(chx)
     for t in x:
         if t == chx:
             dis[i] = t
-            i += 1
+            print(i)
+        i = i + 1
+    print (dis)
+    if chx not in x:
+        global var
+        var -= 1
+        print("ops!!! you have", var,"chances left")
+    if "-" in dis and var != 0:
+        game_play(x, dis)
+
+
     return dis
 
-
-
-x = get_word()
+#y is collecting the randmized word
+y = get_word()
+print(y)
+#a random value is being selected based on the range of the word provided
+ww = randint(0,len(y)-1)
+x = y[ww]
 print(x)
-ww = randint(0,len(x)-1)
-print(display_val(x[ww]))
-dis = display_val(x[ww])
-game_play(ww,dis)
+# a display is being generated
+print(display_val(y[ww]))
+dis = display_val(y[ww])
+# the game has been initialized
+print(game_play(x,dis))
