151c2ff9ae9481051063b8f7b9070eb321fc8dd3
Joshua-amare <41296823+Joshua-amare@users.noreply.github.com>
2019-02-10 21:50:26 +0800

    new udates

--- hang_man.py
+++ hang_man.py
@@ -15,11 +15,30 @@ def get_word():
 
 #creates the - mark for the words based on their length
 def display_val(val):
+    dis = []
     for x in val:
-        print("-",end='')
+        dis.append("-")
+    return dis
 
 #name game_play checks if the user has taken the input supplied
+def game_play(x,dis):
+    #x is the hang man puzzle and dis is the - display
+    temp = []
+    i = 0
+    print("let the game begin but remeber you have to guess the correct words")
+    chx = input(" please write a sigle guess only: ")
+    temp[i]= chx
+    for t in x:
+        if t == chx:
+            dis[i] = t
+            i += 1
+    return dis
+
+
+
 x = get_word()
 print(x)
 ww = randint(0,len(x)-1)
-display_val(x[ww])
+print(display_val(x[ww]))
+dis = display_val(x[ww])
+game_play(ww,dis)
